b84769f6e6cb47b8296c6668b5562c91c4dd473e
redn1njaa <ostap.seryvko@ucu.edu.ua>
2022-11-24 12:13:28 +0200

    initial commit

---
+++ src/test/java/companies/middle/MiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package companies.middle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
