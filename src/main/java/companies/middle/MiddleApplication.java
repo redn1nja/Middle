bd318dc3bd716006b64acd2a4dbc95f2272f4aac
redn1njaa <ostap.seryvko@ucu.edu.ua>
2022-11-26 14:29:48 +0200

    heroku

--- src/main/java/companies/middle/MiddleApplication.java
+++ src/main/java/companies/middle/MiddleApplication.java
@@ -3,6 +3,7 @@ package companies.middle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MiddleApplication {
 
