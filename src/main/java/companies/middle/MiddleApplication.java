cbfa073017705160a8a33986f4e9d902daee30e9
redn1njaa <ostap.seryvko@ucu.edu.ua>
2022-11-26 14:32:48 +0200

    heroku push 2

--- src/main/java/companies/middle/MiddleApplication.java
+++ src/main/java/companies/middle/MiddleApplication.java
@@ -2,13 +2,21 @@ package companies.middle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
+@GetMapping
 public class MiddleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MiddleApplication.class, args);
     }
 
+    public String getStart(){
+        return "Hello";
+    }
 }
+
